60c73ab56c9a69a4ae7378d71ce26e13241974bf
Kirsten Segavac <kirstensegavac@gmail.com>
2022-10-08 17:10:37 +0800

    setup comment model and connection file

---
+++ models/Comment.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const User = require('./user');
+const Post = require('./Post');
+
+class Comment extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+Comment.init(
+  {
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        },
+      },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: Post,
+          key: 'id'
+      },
+   
+    },
+  },
+    {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
